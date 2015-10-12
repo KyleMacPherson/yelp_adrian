95ebefb230542729be7b9adc13e1eaa53a581a8d
Adrian Booth <Adrian.booth17@gmail.com>
2015-10-12 17:37:45 +0100

    Fill out form test passing

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -13,6 +13,17 @@ feature 'restaurants' do
     end
   end
 
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content "No restaurants yet"
