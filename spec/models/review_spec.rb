bd0b2bbb22d4901165ee0c2e0a9af7b3d03a1f7b
kyle macpherson <kyle1@kyles-MacBook-Pro.local>
2015-10-13 17:19:54 +0100

    user sign in and out functionality working

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe Review, type: :model do
   it { should belong_to :restaurant}
+
+  describe Review, type: :model do
+    it "is invalid if the rating is more tha 5" do
+      review = Review.new(rating: 10)
+      expect(review).to have(1).error_on(:rating)
+    end
+  end
 end
